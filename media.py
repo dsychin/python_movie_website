7b0078ffb8c3be7a9674c953cd49500b3814e85c
Desmond Chin <desmond@shiunyean.tk>
2017-08-06 10:21:53 +0800

    updated code to follow pep8 style

--- media.py
+++ media.py
@@ -1,8 +1,20 @@
-# Class which holds information about the movie
 class Movie:
-    def __init__(self, title, poster_image_url, trailer_youtube_url, storyline):
+    """
+    Class Movie takes title, poster image url, youtube trailer url, and
+    storyline as input and stores it.
+    """
+    def __init__(self, title, poster_image_url,
+                 trailer_youtube_url, storyline):
+        """
+        Assigns parameters to instance variables.
+        
+        Args:
+            title (str): Title of the movie.
+            poster_image_url (str): Link to the poster image of the movie.
+            trailer_youtube_url (str): Link to the youtube trailer of movie.
+            storyline (str): Storyline of the movie.
+        """
         self.title = title
         self.poster_image_url = poster_image_url
         self.trailer_youtube_url = trailer_youtube_url
         self.storyline = storyline
-        
